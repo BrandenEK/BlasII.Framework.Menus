697d4821369986a057e87b71c05e8a975cc87a53
Branden Wood <44546325+BrandenEK@users.noreply.github.com>
2025-07-01 16:36:50 -0400

    core: cleanup

--- BlasII.Framework.Menus/MenuFramework.cs
+++ BlasII.Framework.Menus/MenuFramework.cs
@@ -5,11 +5,6 @@ using Il2CppTGK.Game;
 using Il2CppTGK.Game.Components.UI;
 using UnityEngine;
 
-using System.Text;
-using Il2CppTGK.InputSystem;
-using System.Linq;
-using System.Collections.Generic;
-
 namespace BlasII.Framework.Menus;
 
 /// <summary>
@@ -225,42 +220,4 @@ public class MenuFramework : BlasIIMod
         provider.RegisterLoadGameMenu(new TestMenu("Load game menu", 50, false));
     }
 #endif
-}
-
-internal static class InfoExtensions
-{
-    // Recursive method that returns the entire hierarchy of an object
-    public static string DisplayHierarchy(this Transform transform, int maxLevel, bool includeComponents)
-    {
-        return transform.DisplayHierarchy_INTERNAL(new StringBuilder(), 0, maxLevel, includeComponents).ToString();
-    }
-
-    private static StringBuilder DisplayHierarchy_INTERNAL(this Transform transform, StringBuilder currentHierarchy, int currentLevel, int maxLevel, bool includeComponents)
-    {
-        // Indent
-        for (int i = 0; i < currentLevel; i++)
-            currentHierarchy.Append('\t');
-
-        // Add this object
-        currentHierarchy.Append(transform.name);
-
-        // Add components
-        if (includeComponents)
-        {
-            currentHierarchy.Append(" - ");
-            foreach (Component c in transform.GetComponents<Component>())
-                currentHierarchy.Append(c.GetIl2CppType().FullName + ", ");
-        }
-        currentHierarchy.AppendLine();
-
-        // Add children
-        if (currentLevel < maxLevel)
-        {
-            for (int i = 0; i < transform.childCount; i++)
-                currentHierarchy = transform.GetChild(i).DisplayHierarchy_INTERNAL(currentHierarchy, currentLevel + 1, maxLevel, includeComponents);
-        }
-
-        // Return output
-        return currentHierarchy;
-    }
 }
